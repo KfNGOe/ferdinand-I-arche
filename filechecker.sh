9640856d8dd847f4ad8af8587b57504534e1026d
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-06 14:10:29 +0100

    minor

--- filechecker.sh
+++ filechecker.sh
@@ -1,6 +1,14 @@
+# directoryToWriteReportsInto="fc_out"
+
+# echo "run ${directoryToWriteReportsInto}"
+# rm -rf ${directoryToWriteReportsInto}
+# mkdir -p ${directoryToWriteReportsInto}
+# vendor/bin/arche-filechecker --csv --html to_ingest ${directoryToWriteReportsInto}
+
+
 directoryToWriteReportsInto="fc_out"
 
 echo "run ${directoryToWriteReportsInto}"
 rm -rf ${directoryToWriteReportsInto}
 mkdir -p ${directoryToWriteReportsInto}
-vendor/bin/arche-filechecker --csv --html to_ingest ${directoryToWriteReportsInto}
+docker run --rm -v `pwd`/${directoryToWriteReportsInto}:/reports -v `pwd`/to_ingest:/data --entrypoint /opt/filechecker/bin/arche-filechecker acdhch/arche-filechecker --csv --html /data /reports
