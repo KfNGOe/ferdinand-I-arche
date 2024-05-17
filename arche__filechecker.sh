3c13f5ab97614d2f1a51c8b141b28b2ef550e8d1
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-17 12:12:07 +0200

    #11; filechecker

---
+++ arche__filechecker.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# python3 -m pip install --user cvdupdate && cvd update
+
+rm -rf fc_out && mkdir fc_out
+docker run \
+  --rm \
+  -v ${PWD}/fc_out:/reports \
+  -v ${PWD}/to_ingest:/data \
+  -v ~/.cvdupdate/database/:/var/lib/clamav \
+  acdhch/arche-filechecker
