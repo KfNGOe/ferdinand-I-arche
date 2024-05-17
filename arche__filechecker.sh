945400e59ed87a17c7cf9c158fe10c732d7f375f
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-17 12:45:37 +0200

    adding some params

--- arche__filechecker.sh
+++ arche__filechecker.sh
@@ -3,10 +3,9 @@
 # python3 -m pip install --user cvdupdate && cvd update
 
 rm -rf ${PWD}/fc_out && mkdir ${PWD}/fc_out
-ls
 docker run \
   --rm \
   -v ${PWD}/fc_out:/reports \
   -v ${PWD}/to_ingest:/data \
   -v ~/.cvdupdate/database/:/var/lib/clamav \
-  acdhch/arche-filechecker
+  acdhch/arche-filechecker --overwrite --skipWarnings
