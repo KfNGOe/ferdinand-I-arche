31839def6a8a7e325a2f258fb9be68b48f495c14
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-21 13:51:30 +0200

    minor

--- arche__ingest_binaries.sh
+++ arche__ingest_binaries.sh
@@ -7,4 +7,4 @@ rm -rf ${PWD}/fc_out && mkdir ${PWD}/fc_out
 docker run \
   --rm \
   -v ${PWD}/to_ingest:/data \
-  acdhch/arche-filechecker vendor/bin/arche-import-binary to_ingest https://id.acdh.oeaw.ac.at/wollmilchsau ${ARCHE}/api ${ARCHE_USER} ${ARCHE_PW} --skip not_exist
+  acdhch/arche-filechecker vendor/bin/arche-import-binary to_ingest https://id.acdh.oeaw.ac.at/${project} ${ARCHE}/api ${ARCHE_USER} ${ARCHE_PW} --skip not_exist
